a80c9ffb33b457f80e5afc904e6a105f78e687a7
sagar <sag.dobhal@gmail.com>
2025-01-30 19:19:38 +0530

    Assignment3

---
+++ src/com/assignments/EmpGroups.java
@@ -0,0 +1,28 @@
+package com.assignments;
+
+public class EmpGroups extends Employees{
+
+	
+	public static void main(String[] args) {
+	
+		String []empName1 = new String[3];
+		empName1[0]= empName[0];
+		empName1[1]= empName[1];
+		empName1[2]= empName[2];
+	
+	     int []empID2 = new int[3];
+	
+	    empID2[0]= empID[0];
+		empID2[1]= empID[1];
+		empID2[2]= empID[2];
+		
+		
+		System.out.println("Employee Name:"+empName1[0]+","+"Employee ID:"+empID2[0]);
+		System.out.println("Employee Name:"+empName1[1]+","+"Employee ID:"+empID2[1]);
+		System.out.println("Employee Name:"+empName1[2]+","+"Employee ID:"+empID2[2]);
+		
+	
+
+	}
+
+}
---
+++ src/com/assignments/Employees.java
@@ -0,0 +1,14 @@
+package com.assignments;
+
+public class Employees {
+	
+	 static String []empName = {"Sagar","ABC","XYZ"};
+	 static int []empID = {1234,1235,1236};
+
+	public static void main(String[] args) {
+
+      
+
+	}
+
+}
